09352ee834645edde3eed21c101169247a08dd92
Alexandre-Caby <alexandre.caby@ig2i.centralelille.fr (GitHub)>
2025-06-04 11:12:11 +0200

    hotfix

--- app/services/mission.py
+++ app/services/mission.py
@@ -6,7 +6,8 @@ def get_mission(db: Session, mission_id: str):
     return db.query(MissionModel).filter(MissionModel.id == mission_id).first()
 
 def list_missions(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(MissionModel).offset(skip).limit(limit).all()
+    return db.query(MissionModel).filter(MissionModel.status.notin_(["terminated", "failed"])).offset(skip).limit(limit).all()
+    #return db.query(MissionModel).offset(skip).limit(limit).all()
 
 def create_mission(db: Session, mission: MissionCreate):
     db_mission = MissionModel(**mission.dict())
