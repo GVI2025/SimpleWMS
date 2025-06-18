454b4e80b18e289bf828a8cc76df999b0f64e18a
AlexFizl <alexandre.fizel@ig2i.centralelille.fr>
2025-06-18 10:16:43 +0200

    feat: fix for test

--- app/models/reservation.py
+++ app/models/reservation.py
@@ -1,7 +1,6 @@
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String, ForeignKey, Date, Time
 from sqlalchemy.orm import relationship
 from uuid import uuid4
-from datetime import date, time
 
 from app.database.database import Base
 
@@ -10,8 +9,8 @@ class Reservation(Base):
 
     id = Column(String, primary_key=True, default=lambda: str(uuid4()))
     salle_id = Column(String, ForeignKey("salle.id"), nullable=False)
-    date = Column(date, nullable=False)
-    heure = Column(time, nullable=False)
+    date = Column(Date, nullable=False)
+    heure = Column(Time, nullable=False)
     utilisateur = Column(String, nullable=False)
 
     salle = relationship("Salle", foreign_keys=[salle_id])
