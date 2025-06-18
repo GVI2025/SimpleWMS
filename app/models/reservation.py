2d77e8bac5a7b2060bb6aae0c918fbcfe908ca7e
TRETomas <viixyy2@gmail.com>
2025-06-18 09:09:31 +0200

    fix: change type date and time

--- app/models/reservation.py
+++ app/models/reservation.py
@@ -1,7 +1,7 @@
-from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from uuid import uuid4
-from datetime import datetime
+from datetime import date, time
 
 from app.database.database import Base
 
@@ -10,9 +10,8 @@ class Reservation(Base):
 
     id = Column(String, primary_key=True, default=lambda: str(uuid4()))
     salle_id = Column(String, ForeignKey("salle.id"), nullable=False)
-    date = Column(DateTime, nullable=False)
-    heure = Column(Integer, nullable=False)
+    date = Column(date, nullable=False)
+    heure = Column(time, nullable=False)
     utilisateur = Column(String, nullable=False)
-    commentaire = Column(String, nullable=True)
 
     salle = relationship("Salle", foreign_keys=[salle_id])
